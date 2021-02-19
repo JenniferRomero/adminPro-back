d736b838a55b03be7a461dd087a65e0b9c54ab42
Jennifer <ross_424@hotmail.com>
2021-02-18 19:11:11 -0500

    Auth Google SignIn

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@
  const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const usuario = require('../models/usuario');
  
  const login = async(req, res = response) => {
 
@@ -47,6 +49,53 @@ const { generarJWT } = require('../helpers/jwt');
     }
  }
 
+
+ 
+ const googleSignIn = async(req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try {
+
+        const { email, name, picture } = await googleVerify(googleToken);
+        const usuarioDB = await Usuario.findOne({email});
+        let user;
+
+        if (!usuarioDB) {
+            user = new Usuario({
+                email,
+                nombre: name,
+                password: 'fdsf',
+                img: picture,
+                google: true
+            });
+        } else {
+            user = usuarioDB;
+            user.google = true
+        }
+
+        //guardar en base de datos
+        await user.save();
+
+        
+        //Generar un token
+        const token = await generarJWT(user.id);
+
+        res.json({
+            ok: true,
+            token
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            ok: false,
+            msg: 'token no es correcto'
+        })
+    }
+ }
+
  module.exports = {
      login,
+     googleSignIn
  }
